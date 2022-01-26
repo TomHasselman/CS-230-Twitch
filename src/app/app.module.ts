3db302994dcbd344d0eab604924014d46c52c205
Tom Hasselman <thasselm@mix.wvu.edu>
2022-01-25 20:06:57 -0500

    implemented "followed channels" component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navigation/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FollowedChannelsComponent } from './followed-channels/followed-channels.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    FollowedChannelsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/followed-channels/followed-channels.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-followed-channels',
+  templateUrl: './followed-channels.component.html',
+  styleUrls: ['./followed-channels.component.css']
+})
+export class FollowedChannelsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
