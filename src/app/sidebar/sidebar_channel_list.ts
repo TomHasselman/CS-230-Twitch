d7486ae82b0fdeb2225a9cba105950204cb2ce98
Tom Hasselman <thasselm@mix.wvu.edu>
2022-02-08 15:40:43 -0500

    Refactored followed-channels to utilize ngFor loop

--- src/app/sidebar/sidebar_channel_list.ts
+++ src/app/sidebar/sidebar_channel_list.ts
@@ -4,5 +4,23 @@ export const sidebar_channel_list  = [
         name: "Dash Ducks",
         viewers: 347,
         category: "Animals, Aquariums, and Zoos"
+    },
+    {
+        img: "https://static-cdn.jtvnw.net/jtv_user_pictures/84e7f046-5fe1-4bdc-8857-c4570ce346b0-profile_image-70x70.png",
+        name: "RamsReef",
+        viewers: 7,
+        category: "Animals, Aquariums, and Zoos"  
+    },
+    {
+        img: "https://static-cdn.jtvnw.net/jtv_user_pictures/99aa4739-21d6-40af-86ae-4b4d3457fce4-profile_image-70x70.png",
+        name: "summit1g",
+        viewers: 15.5,
+        category: "LOST ARK"
+    },
+    {
+        img: "https://static-cdn.jtvnw.net/jtv_user_pictures/6cd4de40-1a83-46c7-aea5-3bd73f90e7e4-profile_image-70x70.png",
+        name: "pokimane",
+        viewers: 31.2,
+        category: "Just Chatting" 
     }
 ]; 
